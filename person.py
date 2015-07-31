712ef95de42e9d5210e964d608cd5fca74f4ce8c
Alexandr Lark <kvark.lark@gmail.com>
2015-07-31 23:47:44 +0300

    make instances

---
+++ person.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
