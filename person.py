4e5494cde0f899df92b9595ac5eef130b0ac1b9f
Alexandr Lark <kvark.lark@gmail.com>
2015-08-01 00:08:12 +0300

    customize behavior

--- person.py
+++ person.py
@@ -10,6 +10,10 @@ class Person:
     def __str__(self):
         return '[Person: {}, {}]'.format(self.name, self.pay)
 
+class Manager(Person):
+    def giveRaise(self, percent, bonus=.10):
+        Person.giveRaise(self, percent + bonus)
+
 if __name__ == '__main__':
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job='dev', pay=100000)
@@ -18,3 +22,7 @@ if __name__ == '__main__':
     print(bob.lastName(), sue.lastName())
     sue.giveRaise(.10)
     print(sue)
+    tom = Manager('Tom Jones', 'mgr', 50000)
+    tom.giveRaise(.10)
+    print(tom.lastName())
+    print(tom)
