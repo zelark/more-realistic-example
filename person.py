cc8e47a8769922ed779c1cbe816a3b858d4a0b4f
Alexandr Lark <kvark.lark@gmail.com>
2015-07-31 23:51:34 +0300

    add behavior methods

--- person.py
+++ person.py
@@ -3,9 +3,16 @@ class Person:
         self.name = name
         self.job = job
         self.pay = pay
+    def lastName(self):
+        return self.name.split()[-1]
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
 
 if __name__ == '__main__':
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job='dev', pay=100000)
     print(bob.name, bob.pay)
     print(sue.name, sue.pay)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue.pay)
